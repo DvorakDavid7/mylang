669d7f7a6daf8e068d1de51089fa212264ddf055
David Dvořák <dvorakdavid7@gmail.com>
2022-07-27 15:01:41 +0200

    tokenizer

---
+++ src/mylang/tokenizer.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def get_tokens(source_code: str) -> List[str]:
+    tokens: List[str] = []
+
+    striped_lines = _strip_source(source_code)
+
+    for command in striped_lines:
+        operation = command.split(" ")
+        for token in operation:
+            tokens.append(token)
+
+    return tokens
+
+
+def _strip_source(source_code: str) -> List[str]:
+    striped_lines: List[str] = []
+    lines = source_code.split("\n")
+
+    for line in lines:
+        word: str = line.strip()
+        if word != "":
+            striped_lines.append(line.strip())
+
+    return striped_lines
---
+++ test/test_main.py
@@ -0,0 +1,6 @@
+import unittest
+
+class MainTest(unittest.TestCase):
+
+    def test_main(self):
+        self.assertTrue(True)
